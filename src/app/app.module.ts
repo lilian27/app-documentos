81835dc4d5dfec490842c8f0516b9de369bd0a61
lilian patiño <lilian@MacBook-Air-de-lilian.local>
2022-02-01 16:48:57 -0300

    estructura de carpetas base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { HomeComponent } from './modules/home/home.component';
+import { DocumentosComponent } from './modules/documentos/documentos.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    HomeComponent,
+    DocumentosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
